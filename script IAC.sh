b120acbee37564b0ad3fcf87cfdeba3b73785405
unknown <adilson_si@yahoo.com.br>
2022-08-19 10:13:13 -0300

    Primeiro commit

---
+++ script IAC.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "# Atualizando o servidor #"
+echo "--------------------------"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "# Baixando e atualizando os arquivos da aplicação #"
+echo "---------------------------------------------------"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "# Finalizado com sucesso #"
+echo "--------------------------"
+
